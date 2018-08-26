ca6839950b2ec5713a318b8aa04e020f850e81d6
david.vega@snhu.edu <david.vega@snhu.edu>
2018-08-26 17:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ function happyHolidays(){
   return 'Happy holidays!'
 }
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
-}
-function happyHolidaysTo (holiday, name){
-  return 'Happy ${holiday}, ${name}!'
-}
-function holidayCountdown (days, holiday){
-  return `It's ${days} until ${holiday}!`
+  return `Happy holidays to ${name}`
 }
